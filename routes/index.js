6537472bbccfd39328b036ad34182a5145f79351
Jose Jimenez <josejimenez826@gmail.com>
2021-04-27 22:41:40 -0700

    added a few files and routes

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+// const note = require('../db/note');
+
+// router.get("/", (req, res) => {
+//     console.log('working')
+//     res.send('')
+// })
+
+module.exports = router;
