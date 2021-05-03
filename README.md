59688fcf89128191ed3dd73f25cc8ac1792a0afa
Jose Jimenez <josejimenez826@gmail.com>
2021-05-02 23:43:18 -0700

    added inquirer part and readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Employee_Tracker
+
+## Description
+This project is a tracker for employees, departments, and roles in a company. The user is able to view, add, update and delete data from a sql database. All the questions of the project are through the console which the user is able to enter data and select choice they want. This project mainly uses inquirer, sequelize, fetch, and sql all to get data from a database and interact with the user.
+
+## Usage
+To use this project a user will need to run "node server.js" they will then be prompted with a question "What would you like to do?". The user is then able to view either "role", "department", and "employee" and select one of them. Afterwares the user will be prompoted on what they want to do, which then they can add, update, and delete from their first choice. 
+
+# Links 
+https://github.com/JoseJ55/Employee_Tracker
--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-const inquirer = require("inquirer");
-// const sql = require("mysql");
 const express = require('express');
 const routes = require('./routes/index');
-const sequelize = require('./config/connection')
+const routesD = require('./routes/api/deparmentRoutes')
+const sequelize = require('./config/connection');
+const a = require('./lib/index')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// let route = __dirname + "/index";
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(routes); 
 
-// app.get('/', (req, res) => res.send("working"))
- 
 sequelize.sync({force: false}).then(()=>{
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`))
 }); 
 
-// Comment for important things
-//-----------------------------------
-// Need to work on the inquirer part next.
-//-----------------------------
+a.ask();
+
+
